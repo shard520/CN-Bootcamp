03db66ba3fc2b312f976ce348f0a244eccebba58
shard520 <65344196+shard520@users.noreply.github.com>
2021-10-20 13:44:57 +0100

    Add React state practice.

--- week-5/state-practice/src/App.js
+++ week-5/state-practice/src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [nums, setNums] = useState([1, 2, 3]);
+  const [value, setValue] = useState('');
+  const [submittedText, setSubmittedText] = useState('');
+
+  const handleAdd = () => {
+    setCount(count + 1);
+  };
+
+  const handleMinus = () => {
+    setCount(count - 1);
+  };
+
+  const handleAddNum = () => {
+    const storedNums = [...nums];
+    storedNums.push(storedNums[storedNums.length - 1] + 1);
+    setNums(storedNums);
+  };
+
+  const handleDeleteNum = index => {
+    console.log(index);
+
+    const storedNums = [...nums];
+    storedNums.splice(index, 1);
+    setNums(storedNums);
+  };
+
+  const handleInputChange = e => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setSubmittedText(value);
+    setValue('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter count={count} add={handleAdd} subtract={handleMinus} />
+      <NumList
+        numbers={nums}
+        addNum={handleAddNum}
+        deleteNum={handleDeleteNum}
+      />
+      <Input
+        val={value}
+        update={handleInputChange}
+        submit={handleSubmit}
+        text={submittedText}
+      />
     </div>
   );
 }
+
+const Counter = ({ count, add, subtract }) => {
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={add}>Add One</button>
+      <button onClick={subtract}>Minus One</button>
+    </div>
+  );
+};
+
+const NumList = ({ numbers, addNum, deleteNum }) => {
+  return (
+    <div>
+      <p>
+        {numbers.map((num, i) => {
+          return (
+            <span onClick={() => deleteNum(i)} key={i}>
+              {num}{' '}
+            </span>
+          );
+        })}
+      </p>
+      <button onClick={addNum}>Add another number</button>
+    </div>
+  );
+};
+
+const Input = ({ val, update, submit, text }) => {
+  return (
+    <div>
+      <input type="text" value={val} onChange={update} />
+      <button onClick={submit}>Submit</button>
+      <p>You have submitted: {text}</p>
+    </div>
+  );
+};
 
 export default App;
