b3c16326bdce51701a950d36b92f820df0899ae7
shard520 <65344196+shard520@users.noreply.github.com>
2021-11-01 17:07:18 +0000

    node-practice: add more ways to input data, use optional chaining for unrequired fields, add usage and commands information.

--- week-7/node-practice/src/index.js
+++ week-7/node-practice/src/index.js
@@ -1,17 +1,19 @@
-const yargs = require('yargs');
-const { hideBin } = require('yargs/helpers');
-const argv = yargs(hideBin(process.argv)).argv;
+const argv = require('yargs/yargs')(process.argv.slice(2))
+  .usage('Usage: $0 <command> [options]')
+  .command('--add', 'Add a single movie')
+  .command('--multi', 'Add multiple movies').argv;
 
 const { Movie, moviesArray } = require('./utils');
 
 const app = () => {
-  console.log(argv);
   if (argv.add) {
     let newMovie;
 
     if (argv.title) {
+      // single movie with title flag
       newMovie = new Movie(argv.title, argv?.actor);
     } else {
+      // single movie with dot notation
       newMovie = new Movie(argv.movie.title, argv.movie?.actor);
     }
 
@@ -20,21 +22,30 @@ const app = () => {
     let newMovie1, newMovie2;
 
     if (Array.isArray(argv.actor)) {
+      // multiple actors passed individually
       newMovie1 = new Movie(argv.title[0], argv.actor[0]);
       newMovie2 = new Movie(argv.title[1], argv.actor[1]);
     } else if (typeof argv.actor === 'string') {
+      // actor only given for 1st movie
       newMovie1 = new Movie(argv.title[0], argv.actor);
       newMovie2 = new Movie(argv.title[1]);
     } else if (argv.movie1 && argv.movie2) {
+      // multiple movies passed with dot notation, optional chaining to allow actors to be passed or omitted
       newMovie1 = new Movie(argv.movie1.title, argv.movie1?.actor);
       newMovie2 = new Movie(argv.movie2.title, argv.movie2?.actor);
     } else {
+      // multiple movies with no actors
       newMovie1 = new Movie(argv.title[0]);
       newMovie2 = new Movie(argv.title[1]);
     }
 
     newMovie1.add();
     newMovie2.add();
+  } else if (!argv.add && !argv.multi) {
+    console.log(
+      'Please specify --add to add a single movie or --multi to add multiple movies'
+    );
+    return;
   }
 
   console.table(moviesArray);
