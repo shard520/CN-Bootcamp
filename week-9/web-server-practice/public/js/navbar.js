7c8df0d10bd336f1ee64ea2019eefa9c90f4b330
shard520 <65344196+shard520@users.noreply.github.com>
2021-11-22 11:44:03 +0000

    Add web-server-practice

---
+++ week-9/web-server-practice/public/js/navbar.js
@@ -0,0 +1,9 @@
+const navbar = document.querySelector('.navbar');
+
+navbar.innerHTML = `
+<ul class="navbar__list">
+  <li class="navbar__item"><a class="navbar__link" href="index.html">Home</a></li>
+  <li class="navbar__item"><a class="navbar__link" href="about.html">About</a></li>
+  <li class="navbar__item"><a class="navbar__link" href="contact.html">Contact</a></li>
+</ul>
+`;
